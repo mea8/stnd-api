4c93edc0665ae10a81cb53cd5771d8c66312d823
mea <meywrt7@gmail.com>
2018-08-28 00:54:05 +0200

    r

--- src/main/java/com/stnd/stndapi/model/User.java
+++ src/main/java/com/stnd/stndapi/model/User.java
@@ -3,7 +3,7 @@ package com.stnd.stndapi.model;
 public class User {
     private String name;
     private int age;
-    int a;
+    private int a;
 
     public String getName() {
         return name;
